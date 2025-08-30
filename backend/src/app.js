ebebb9365275cf0d8599268f8e0bbfa91095b24b
chetram <patelchetram49@gmail.com>
2025-08-30 11:29:30 +0530

    fix:cors error

--- backend/src/app.js
+++ backend/src/app.js
@@ -9,12 +9,10 @@ const app = express();
 const cookieParser = require('cookie-parser')
 
 
-
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: process.env.CORS_ORIGIN || "http://localhost:5173",
   credentials: true
 }));
-
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
