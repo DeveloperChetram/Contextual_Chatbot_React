44658f0f29ca5e4134f3b60975c6c7881965e4e7
develoeprchetram <patelchetram49@gmail.com>
2026-07-22 17:12:07 +0530

    fix: socket  time

--- backend/src/utils/cookieOptions.js
+++ backend/src/utils/cookieOptions.js
@@ -1,7 +1,9 @@
+const isProduction = process.env.NODE_ENV === 'production';
+
 const cookieOptions = {
     httpOnly: true,
-    secure: process.env.NODE_ENV === 'production', 
-    sameSite: process.env.NODE_ENV === 'production' ? 'None' : 'Lax',
+    secure: true, // Ensure cookies are only sent over HTTPS
+    sameSite:isProduction ? 'none' : 'lax',
     maxAge: 5 * 24 * 60 * 60 * 1000, // 5 days in milliseconds
 };
 
